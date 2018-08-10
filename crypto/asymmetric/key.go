b3e40b160c0ddd1e8a4009797b3ad11282e06028
Philipp Brüll <pb@simia.tech>
2018-08-10 16:47:38 +0200

    changed key encoding from hex to base64

--- crypto/asymmetric/key.go
+++ crypto/asymmetric/key.go
@@ -5,13 +5,15 @@ import (
 	"crypto/rand"
 	"crypto/rsa"
 	"crypto/x509"
-	"encoding/hex"
+	"encoding/base64"
 
 	"github.com/simia-tech/errx"
 )
 
 type PublicKey []byte
 type PrivateKey []byte
+
+var encoding = base64.RawStdEncoding.Strict()
 
 func GenerateKeyPair() (PublicKey, PrivateKey, error) {
 	privateKey, err := rsa.GenerateKey(rand.Reader, 512)
@@ -26,27 +28,27 @@ func GenerateKeyPair() (PublicKey, PrivateKey, error) {
 }
 
 func (pk PublicKey) MarshalJSON() ([]byte, error) {
-	buffer := make([]byte, hex.EncodedLen(len(pk)))
-	hex.Encode(buffer, pk)
+	buffer := make([]byte, encoding.EncodedLen(len(pk)))
+	encoding.Encode(buffer, pk)
 	return bytes.Join([][]byte{[]byte("\""), buffer, []byte("\"")}, []byte{}), nil
 }
 
 func (pk *PublicKey) UnmarshalJSON(data []byte) error {
 	data = data[1 : len(data)-1]
-	*pk = make([]byte, hex.DecodedLen(len(data)))
-	_, err := hex.Decode(*pk, data)
+	*pk = make([]byte, encoding.DecodedLen(len(data)))
+	_, err := encoding.Decode(*pk, data)
 	return err
 }
 
 func (pk PrivateKey) MarshalJSON() ([]byte, error) {
-	buffer := make([]byte, hex.EncodedLen(len(pk)))
-	hex.Encode(buffer, pk)
+	buffer := make([]byte, encoding.EncodedLen(len(pk)))
+	encoding.Encode(buffer, pk)
 	return bytes.Join([][]byte{[]byte("\""), buffer, []byte("\"")}, []byte{}), nil
 }
 
 func (pk *PrivateKey) UnmarshalJSON(data []byte) error {
 	data = data[1 : len(data)-1]
-	*pk = make([]byte, hex.DecodedLen(len(data)))
-	_, err := hex.Decode(*pk, data)
+	*pk = make([]byte, encoding.DecodedLen(len(data)))
+	_, err := encoding.Decode(*pk, data)
 	return err
 }
--- crypto/symmetric/key.go
+++ crypto/symmetric/key.go
@@ -1,11 +1,30 @@
 package symmetric
 
-import "crypto/rand"
+import (
+	"bytes"
+	"crypto/rand"
+	"encoding/base64"
+)
 
 type Key []byte
+
+var encoding = base64.RawStdEncoding.Strict()
 
 func GenerateKey() Key {
 	key := make([]byte, 32)
 	rand.Read(key)
 	return key
+}
+
+func (k Key) MarshalJSON() ([]byte, error) {
+	buffer := make([]byte, encoding.EncodedLen(len(k)))
+	encoding.Encode(buffer, k)
+	return bytes.Join([][]byte{[]byte("\""), buffer, []byte("\"")}, []byte{}), nil
+}
+
+func (k *Key) UnmarshalJSON(data []byte) error {
+	data = data[1 : len(data)-1]
+	*k = make([]byte, encoding.DecodedLen(len(data)))
+	_, err := encoding.Decode(*k, data)
+	return err
 }
